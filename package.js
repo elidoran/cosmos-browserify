c0b4c6b67d450224f0346ee79936b64a2ba0bc97
Eli Doran <eli@elidoran.com>
2015-05-06 23:29:48 -0400

    enable testing plugin file with mocks

--- package.js
+++ package.js
@@ -8,6 +8,7 @@ Package.describe({
 
 Package.registerBuildPlugin({
   name: "cosmosBrowserify",
+  // need 'meteor' for Npm and Meteor.wrapAsync
   use: ['coffeescript@1.0.6','meteor'],
   sources: ['plugin/browserify.coffee'],
   npmDependencies: {"browserify": "9.0.8"}
@@ -15,6 +16,17 @@ Package.registerBuildPlugin({
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('cosmos:browserify');
-  api.addFiles('test/browserify-tests.js');
+  // not testing by adding package in 'use'
+  api.use('coffeescript@1.0.6');
+  api.addFiles([
+    // export stuff for use in Testing, and to Mock things like Npm
+    //'test/export.js',
+    // fill in the exported mocks (separate so I can write in CoffeScript)
+    'test/mocks.coffee',
+    // the tests
+    'test/browserify-tests.coffee',
+    // add our plugin file directly instead of adding package in api.use
+    'plugin/browserify.coffee'
+  ], 'client');
+
 });
