b4b6a7658764d5485cf5a34eb0dfd341a4a66fec
Eli Doran <eli@elidoran.com>
2015-06-21 17:28:24 -0400

    cleanup and +0.4.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cosmos:browserify',
-  version: '0.3.0',
+  version: '0.4.0',
   summary: 'Bundle NPM modules for client side with Browserify',
   git: 'https://github.com/elidoran/cosmos-browserify.git',
   documentation: 'README.md'
@@ -11,17 +11,17 @@ Package.registerBuildPlugin({
   // need 'meteor' for Npm and Meteor.wrapAsync
   use: ['coffeescript@1.0.6', 'meteor', 'underscore'],
   sources: ['plugin/browserify.coffee'],
-  npmDependencies: {"browserify": "9.0.8", "envify": "3.4.0", "exorcist":"0.4.0"}
+  npmDependencies: {"browserify": "9.0.8", "envify":"3.4.0", "exorcist":"0.4.0"}
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  // not testing by adding package in 'use'
-  api.use(['coffeescript@1.0.6', 'underscore']);
+
+  // not testing by adding package in 'use'.
+  // using mocks to test only this package's functionality
+  api.use(['tinytest', 'coffeescript@1.0.6', 'underscore']);
+
   api.addFiles([
-    // export stuff for use in Testing, and to Mock things like Npm
-    //'test/export.js',
-    // fill in the exported mocks (separate so I can write in CoffeScript)
+    // fill in the exported mocks (separate so I can write in CoffeeScript)
     'test/mocks.coffee',
     // the tests
     'test/browserify-tests.coffee',
