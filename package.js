7b4fb79ee2b4f683ed6cae919073512314b11197
Eli Doran <eli@elidoran.com>
2015-09-22 18:59:41 -0400

    specify version of underscore

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "CosmosBrowserify",
   // need 'meteor' for Npm and Meteor.wrapAsync
-  use: ['caching-compiler@1.0.0', 'coffeescript@1.0.9', 'meteor', 'underscore'],
+  use: ['caching-compiler@1.0.0', 'coffeescript@1.0.9', 'meteor', 'underscore@1.0.4'],
   sources: ['plugin/browserify.coffee'],
   npmDependencies: {"browserify": "11.1.0", "envify":"3.4.0", "exorcist":"0.4.0"}
 });
