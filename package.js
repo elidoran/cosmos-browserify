eb6afee7bb2eba79bf1c9a01bf5ac266fca1c631
Eli Doran <eli@elidoran.com>
2015-10-30 21:24:34 -0400

    upgrade Browserify from 11.1.0 to 11.2.0

--- package.js
+++ package.js
@@ -9,10 +9,10 @@ Package.describe({
 Package.registerBuildPlugin({
   name: "CosmosBrowserify",
   // need 'meteor' for Npm and Meteor.wrapAsync
-  use: ['caching-compiler@1.0.0', 'coffeescript@1.0.10', 'meteor', 'underscore@1.0.4'],
+  use: ['caching-compiler@1.0.0', 'coffeescript@1.0.11', 'meteor', 'underscore@1.0.4'],
   sources: ['plugin/browserify.coffee'],
   npmDependencies: {
-    "browserify": "11.1.0",     // primary tool which actually does the browserify
+    "browserify": "11.2.0",     // primary tool which actually does the browserify
     "envify":"3.4.0",           // transforms process.env values
     "exorcist-stream":"0.4.0",  // gets source map from source (forked version for streaming)
     "strung":"1.0.4"            // acts as a readable/writable/duplex
@@ -23,7 +23,7 @@ Package.registerBuildPlugin({
 // the list of them in Package.registerBuildPlugin doesn't do it...
 // because plugin goes to .npm/plugin/CosmosBrowserify and these to: .npm/package
 // I'm using a symlink so there's only one copy of them.
-Npm.depends({"browserify": "11.1.0", "envify":"3.4.0", "exorcist-stream":"0.4.0","strung":"1.0.4"});
+Npm.depends({"browserify": "11.2.0", "envify":"3.4.0", "exorcist-stream":"0.4.0","strung":"1.0.4"});
 
 // Need this for the 'isobuild:compiler-plugin'
 Package.onUse(function (api) {
